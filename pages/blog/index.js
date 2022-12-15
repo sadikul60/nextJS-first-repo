3496c562d9e6e78ffbf68e1e3ba902737633e425
mdsadikul <mdsadikulislam891@gamil.com>
2022-12-15 22:50:10 +0600

    adde router

---
+++ pages/blog/index.js
@@ -0,0 +1,12 @@
+
+const index = () => {
+    return (
+        <div>
+            <h1>Blog 1</h1>
+            <h1>Blog 2</h1>
+            <h1>Blog 3</h1>
+        </div>
+    );
+};
+
+export default index;
