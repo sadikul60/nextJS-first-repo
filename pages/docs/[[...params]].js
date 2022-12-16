076ecc5fea5ed086707eaf49d3cfec1044d17982
mdsadikul <mdsadikulislam891@gamil.com>
2022-12-16 09:19:42 +0600

    added multiple route

---
+++ pages/docs/[[...params]].js
@@ -0,0 +1,33 @@
+import { useRouter } from "next/router";
+
+function Document() {
+
+    const router = useRouter();
+    
+    const params = router.query.params || [];
+    console.log(params);
+
+    if (params.length == 1){
+        return (
+            <h2>
+                This page for {params[0]}
+            </h2>
+        )
+    }
+
+    if (params.length == 2){
+        return (
+            <h2>
+                {" "}
+                This page for {params[0]} and {params[1]}{" "}
+            </h2>
+        )
+    }
+    return (
+        <div>
+            <h1>Document Home page</h1>
+        </div>
+    )
+}
+
+export default Document;
